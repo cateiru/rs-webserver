2a84fe0f67179e497d63958fb25b4aee956f41a5
Yuto Watanabe <yuto.w51942@gmail.com>
2022-06-29 16:26:38 +0900

    create

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# rs-webserver
+# Rust WebServer
+
 Sample Web Server written in Rust.
+
+ref. [https://doc.rust-jp.rs/book-ja/ch20-00-final-project-a-web-server.html](https://doc.rust-jp.rs/book-ja/ch20-00-final-project-a-web-server.html)
+
+## LICENSE
+
+[MIT](./LICENSE)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        // println!("接続")
+        handler_connection(stream);
+    }
+}
+
+fn handler_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let mut file = File::open("hello.html").unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]))
 }
