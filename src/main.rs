04ea05ee496de4e9a39dda63e03ea59586b76293
Yuto Watanabe <yuto.w51942@gmail.com>
2022-06-29 16:30:37 +0900

    create single thread server

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,30 @@ fn handler_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
-    let mut file = File::open("hello.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
+    if buffer.starts_with(get) {
+        let mut file = File::open("hello.html").unwrap();
 
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]))
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+
+        println!("Request: {}", String::from_utf8_lossy(&buffer[..]))
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let mut file = File::open("404.html").unwrap();
+        let mut contents = String::new();
+
+        file.read_to_string(&mut contents).unwrap();
+
+        let response = format!("{}{}", status_line, contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
