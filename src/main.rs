0926b285ba08488697b805f212082fed4b920bbf
Yuto Watanabe <yuto.w51942@gmail.com>
2022-06-29 17:28:42 +0900

    create

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
+use rs_webserver::ThreadPool;
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         // println!("接続")
-        handler_connection(stream);
+
+        pool.execute(|| {
+            handler_connection(stream);
+        });
     }
+
+    println!("Shutting down.");
 }
 
 fn handler_connection(mut stream: TcpStream) {
@@ -19,29 +29,24 @@ fn handler_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    if buffer.starts_with(get) {
-        let mut file = File::open("hello.html").unwrap();
-
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).unwrap();
-
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-
-        println!("Request: {}", String::from_utf8_lossy(&buffer[..]))
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-        let mut file = File::open("404.html").unwrap();
-        let mut contents = String::new();
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
 
-        file.read_to_string(&mut contents).unwrap();
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
 
-        let response = format!("{}{}", status_line, contents);
+    file.read_to_string(&mut contents).unwrap();
 
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
